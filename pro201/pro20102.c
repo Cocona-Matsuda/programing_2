c45bec4b72b039d08e0e4b3c5bce324dcd9b0c2a
Cocona.M <53136890+Cocona-Matsuda@users.noreply.github.com>
2021-04-16 13:53:57 +0900

    Add files via upload

---
+++ pro201/pro20102.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main (void){
+    printf("晴\nれ\n");
+
+    return 0;
+}
---
+++ pro201/pro20103.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void){
+    int x = 10;
+
+    printf("%d",x * 3);
+
+    return 0;
+}
