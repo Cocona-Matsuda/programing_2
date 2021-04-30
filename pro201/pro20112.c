1c12fbcd4c33c2cc199319fca7d3e6c86b41e379
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-04-30 10:49:42 +0900

    ?

---
+++ pro201/pro20112.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main (void){
+    int number;
+    int i;
+
+    prirntf("値：");
+    scanf("%d",&number);
+
+    for ( i = number; i < i; i++)
+    {
+        for ( i = 0; i < count; i++)
+        {
+            printf("%d")
+        }
+        
+        printf("%d",i);
+    }
+    
+
+    return 0;
+}
