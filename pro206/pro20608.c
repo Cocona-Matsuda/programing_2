a4299b98aba7b384ccff9b5d82a7af54e0c3f990
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-05-07 12:11:35 +0900

    ?

---
+++ pro206/pro20608.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main (void){
+    char str1[100];
+    char str2[100];
+
+    printf("文字列１：");
+    scanf("%s",str1);
+
+    printf("文字列２：");
+    scanf("%s",str2);
+
+    return 0;
+}
