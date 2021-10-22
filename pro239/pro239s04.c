ea8b268af319f1fd4c9f6ca8b2368abde65aecdf
Cocona-Matsuda <skyebell13rei@gmail.com>
2021-10-22 12:53:41 +0900

    nazenazenazenaze

--- pro239/pro239s04.c
+++ pro239/pro239s04.c
@@ -1,29 +1,32 @@
+//pro239s04.c
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
-int main (void){
-    int *p;
-    int n;
-    int i;
-    int sum;
-
-    printf("配列の要素数：");
-    scanf("%d", &n);
-
-    p = (int *)malloc(sizeof(int) * n);
-    if(p == NULL){
-        exit(EXIT_FAILURE);
-    }
-
-    sum = 0;
-    for ( i = 0; i < n; i++)
-        printf(">>");
-        scanf("%d", &p[i]);^1、
-        sum += p[i];
-    }
-    printf("平均値： %f\n", (float)sum / n);
-    free(p);
-
-    return 0;
+int main(void)
+{
+	int *p;
+	int n;
+	int i;
+	int sum;
+	
+	printf("配列の要素数>> ");
+	scanf("%d", &n);
+	
+	p = (int *)malloc(sizeof(int) * n);
+	if (p == NULL){
+		exit(EXIT_FAILURE);
+	}
+	
+	sum = 0;
+	for (i = 0; i < n; i++){
+		printf(">> ");
+		scanf("%d", &p[i]);
+		//scanf("%d", p + i);
+		sum += p[i];
+	}
+	
+	printf("平均値：%f\n", (float)sum / n);
+	
+	free(p);
+	return (0);
 }
